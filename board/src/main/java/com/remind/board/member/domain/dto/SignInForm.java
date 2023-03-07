2dbe6175b957e362c819c09571123bda6aaf11c2
kimsolee95 <ahhasolee@naver.com>
2023-03-07 23:11:32 +0900

    feat: [001-SIGN-VALIDATION] 회원가입, 로그인 form 필드 validation 추가

--- board/src/main/java/com/remind/board/member/domain/dto/SignInForm.java
+++ board/src/main/java/com/remind/board/member/domain/dto/SignInForm.java
@@ -1,5 +1,7 @@
 package com.remind.board.member.domain.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +13,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SignInForm {
 
+  @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
   private String email;
+
+  @NotBlank(message = "비밀번호는 필수값입니다.")
   private String password;
 }
